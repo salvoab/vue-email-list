04a6920c91bcfd6b6cf836fe42e31fbee53c2173
salvoab <salvatore.abate85@gmail.com>
2021-01-08 16:35:17 +0100

    Cliccando sulle caselle della griglia si ottengono i numeri casuali
    Al click su una casella della griglia si ottiene un numero, e viene assegnata una classe in base al valore numerico ricevuto

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -13,7 +13,9 @@ let app = new Vue({
         getRandomNumber(index){
             axios.get('https://flynn.boolean.careers/exercises/api/random/int')
                 .then(response =>{
-                    this.randomNumbers[index] = response.data.response;
+                    // Uso splice per modificicare l'elemento in posizione index
+                    // se uso direttamente la notazione randomNumbers[index], Vue non vedrà il cambiamento dell'array e non renderizzerà i nuovi valori 
+                    this.randomNumbers.splice(index, 1, response.data.response)
                 })
                 .catch(error => {
                     console.log(error);
