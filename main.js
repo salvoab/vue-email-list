f9e0296fa7754eafc0f74f9e51ae5843b4ed792c
salvoab <salvatore.abate85@gmail.com>
2021-01-08 14:59:43 +0100

    Generata lista di email tramite API e stampata
    Utilizzata API tramite axios per generare email casuali, le email generate vengono mostrate in html in una ol

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 let app = new Vue({
     el: '#app',
     data:{
-
+        emails: []
     },
     mounted(){
-        console.log('test');
+        const emailsQuantity = 10;
+        for(let i=0; i<emailsQuantity; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response =>{
+                    this.emails.push(response.data.response);
+                })
+        }
     }
 });
