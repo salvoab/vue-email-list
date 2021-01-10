93ceb6a7cf9e5c5acd81682ed55785561f239c2c
salvoab <salvatore.abate85@gmail.com>
2021-01-10 12:45:49 +0100

    Richieste concorrenti gestite con Promise.all

--- main.js
+++ main.js
@@ -5,14 +5,15 @@ let app = new Vue({
     },
     mounted(){
         const emailsQuantity = 10;
+        const emailsRequests = [];
+        // Faccio 'emailsQuantity' richieste get concorrenti
         for(let i=0; i<emailsQuantity; i++){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then(response =>{
-                    this.emails.push(response.data.response);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
+           emailsRequests.push(axios.get('https://flynn.boolean.careers/exercises/api/random/mail'));
         }
+
+        // Aggiungo le emails nell'array this.emails dopo che tutte le richieste get concorrenti hanno dato una risposta
+        Promise.all(emailsRequests)
+            .then(results => results.forEach(result => this.emails.push(result.data.response)))
+            .catch(error => console.log(error));
     }
 });
